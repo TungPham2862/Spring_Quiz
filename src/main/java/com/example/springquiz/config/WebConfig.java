f4e1d10b8ae7adae015d96465fe17f482aac2727
Tung Pham <tungpham2862@gmail.com>
2024-10-24 09:50:31 +0700

    deploy mysql credential change

--- src/main/java/com/example/springquiz/config/WebConfig.java
+++ src/main/java/com/example/springquiz/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8080","https://spring-test-1.onrender.com/") // Adjust for your Swagger UI host
+                        .allowedOrigins("http://localhost:8080","https://spring-test-1.onrender.com") // Adjust for your Swagger UI host
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
